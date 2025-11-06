9482be91dd7bdf3a77d2a803d02253822606026e
j3wlius <tamalejulius5@gmail.com>
2025-11-06 21:27:11 +0300

    Go on...

---
+++ myapp/helpers/helper.go
@@ -0,0 +1,5 @@
+package helpers
+
+func Greet(name string) string {
+	return "Hello, " + name + "!"
+}
---
+++ myapp/main.go
@@ -0,0 +1,12 @@
+package main
+
+// import "fmt"
+import (
+	"fmt"
+	"myapp/helpers"
+)
+
+
+func main(){
+	fmt.Println(helpers.Greet("Alice"))
+}
